2dd707125cabf78e4ff51379a682837dd9ea0e37
kuzentio <0585ec@gmail.com>
2017-06-24 11:22:50 +0300

    remove extras from url conf

--- crawler/urls.py
+++ crawler/urls.py
@@ -18,6 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    # url(r'^$', include('pdf-parser.urls', namespace='pdf-parser')),
     url(r'^documents/', include('pdf-parser.urls', namespace='pdf-parser')),
 ]
